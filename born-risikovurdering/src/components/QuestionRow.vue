554fb7cca1aff12b0abd08018dfad4ce0968a869
Fred158f <frederikc@vanrinkelsland.dk>
2024-12-19 00:25:34 +0100

    - Added vue.js framework - Created QuestionRow.vue

---
+++ born-risikovurdering/src/components/QuestionRow.vue
@@ -0,0 +1,19 @@
+<template>
+        <tr>
+            <td>{{question}}</td>
+            <td><input type="text" :placeholder="placeholder"></td>
+            <td><input type="checkbox"><label>NEJ</label></td>
+        </tr>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        question: {type: String, required: true},
+        placeholder: {type: String, required: true},
+    }
+})
+
+</script>
---
+++ born-risikovurdering/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <main>
+    <h2>QUE1 Risikovurdering</h2>
+    <table>
+        <QuestionRow question="Antal timer siden sidste udløsning?" placeholder="Indtast antal timer"/>
+        <QuestionRow question="Har du haft feber, været syg eller været hos lægen?" placeholder="Hvornår og hvorfor?"/>
+        <QuestionRow question="Er du blevet vaccineret?" placeholder="Hvilken vaccine og hvornår?"/>
+        <QuestionRow question="Har du haft ubeskyttet sex med en ny seksualpartner?" placeholder="Hvornår?"/>
+        <QuestionRow question="Har du haft sex med en prostitueret siden sidste donation?" placeholder="Hvornår og var det beskyttet/ubeskyttet?"/>
+        <QuestionRow question="Har du været på rejse til udlandet siden sidste donation?" placeholder="Hvorhenne og hvornår var du i DK igen?"/>
+        <QuestionRow question="Har du fået nye piercinger, tatoveringer eller lignende?" placeholder="Hvornår?"/>
+        <QuestionRow question="Har du kønsvorter, udflåd eller sår på kønsorganerne?" placeholder="Udfyld her"/>
+        <QuestionRow question="Har du taget stoffer siden sidste donation?<br>Hash, ecstasy, MDMA, LSD eller andet?" placeholder="Hvilke og hvornår?"/>
+    </table>
+    <p>QUE1 Risikovurdering<br>Version 2 (30.09.2024)</p>
+  </main>
+</template>
+
+<script lang="ts">
+import QuestionRow from '@/components/QuestionRow.vue';
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        components: {
+            QuestionRow
+        }
+    })
+
+
+</script>
